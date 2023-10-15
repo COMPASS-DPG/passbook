3173a6585a1d9881cb2bee50cf7e8bebc4284b89
faisalEsMagico <faisal.khan@esmagico.in>
2023-10-15 21:42:20 +0530

    feat(error): added error message for pdf download

--- src/components/competency/SubNavbar.tsx
+++ src/components/competency/SubNavbar.tsx
@@ -1,21 +1,52 @@
 'use client';
-import { PDFDownloadLink } from '@react-pdf/renderer';
+import { pdf } from '@react-pdf/renderer';
+import { saveAs } from 'file-saver';
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useState } from 'react';
 import { BsShare } from 'react-icons/bs';
 import { LuDownload } from 'react-icons/lu';
 
 import NavBarLink from '@/components/competency/NavBarLink';
+import DownloadFailError from '@/components/errorScreen/DownloadFailError';
+import ErrorModal from '@/components/errorScreen/ErrorModal';
 import { outfit } from '@/components/FontFamily';
 import PassbookPdfDownload from '@/components/Pdf/PassbookPdfDownload';
 import usePDFData from '@/components/Pdf/usePdfData';
 
 const SubNavbar = () => {
+  // to show pdf download error
+  const [isOpen, setIsOpen] = useState(false);
+
   const router = useRouter();
+
   const { pdfFeedback, pdfData, userName, userId } = usePDFData();
+
+  const handleDownload = async () => {
+    try {
+      const blob = await pdf(
+        <PassbookPdfDownload
+          pdfFeedback={pdfFeedback}
+          pdfData={pdfData}
+          userId={userId}
+          userName={userName}
+        />
+      ).toBlob();
+      saveAs(blob, 'competency');
+    } catch (error) {
+      if (error) {
+        // to show pdf download error modal
+        setIsOpen(true);
+      }
+    }
+  };
 
   return (
     <div className={`sticky top-0 z-10 bg-white  ${outfit.className}`}>
+      {/* modal to show pdf download error */}
+      <ErrorModal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <DownloadFailError onClose={() => setIsOpen(false)} />
+      </ErrorModal>
+
       <div className='flex items-center justify-between p-[20px] text-white'>
         <div className='text-xl font-semibold text-[#272728]'>
           Competency Passbook
@@ -28,28 +59,13 @@ const SubNavbar = () => {
           >
             <BsShare size={24} />
           </button>
-          <PDFDownloadLink
-            document={
-              <PassbookPdfDownload
-                pdfFeedback={pdfFeedback}
-                pdfData={pdfData}
-                userId={userId}
-                userName={userName}
-              />
-            }
-            fileName='form'
-          >
-            {({ loading }) =>
-              !loading && (
-                <button
-                  className='flex h-10 w-10 items-center justify-center
+          <button
+            className='flex h-10 w-10 items-center justify-center
               rounded-md   bg-[#385B8B]'
-                >
-                  <LuDownload size={24} />
-                </button>
-              )
-            }
-          </PDFDownloadLink>
+            onClick={handleDownload}
+          >
+            <LuDownload size={24} />
+          </button>
         </div>
       </div>
       <nav className='mx-[10px]  p-[10px] '>
