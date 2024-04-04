e4bc68908d8907e0b630084c5bf8cb00bd278a42
kh4l1d64 <khalid.m@esmagico.in>
2024-04-04 14:48:27 +0530

    feat: user service integration

--- mockService/userOrg.ts
+++ mockService/userOrg.ts
@@ -2,18 +2,63 @@
 import axios from 'axios';
 
 // Define the base URL of the API endpoint
-const baseUrl = 'https://test-compass.free.beeceptor.com/user';
+// const baseUrl = 'https://test-compass.free.beeceptor.com/user';
 
 // Create a function to fetch user data by user ID
 const fetchUserById = async (userId: string) => {
   try {
     // Make a GET request to the API by appending the user ID to the base URL
-    const response = await axios.get(`${baseUrl}/${userId}`);
+
+    const headers = {
+      Authorization: 'bearer ' + process.env.USER_SERVICE_TOKEN,
+      'Content-Type': 'application/json',
+    };
+
+    const data = {
+      request: {
+        filters: {
+          id: userId,
+        },
+      },
+    };
+
+    const response = await axios.post(
+      'https://compass-dev.tarento.com/api/user/v4/user/search',
+      data,
+      { headers }
+    );
 
     // Check if the request was successful (status code 200)
     if (response.status === 200) {
       // Parse and return the user data
-      return response.data;
+      return {
+        name:
+          response.data.result?.response?.content[0]?.profileDetails
+            ?.personalDetails?.firstname +
+          ' ' +
+          response.data.result?.response?.content[0]?.profileDetails
+            ?.personalDetails?.lastname,
+        phone: response.data.result?.response?.content[0]?.phone,
+        email:
+          response.data.result?.response?.content[0]?.profileDetails
+            ?.personalDetails?.primaryEmail,
+        rootOrganization: {
+          team: {
+            name: response.data.result?.response?.content[0]?.profileDetails
+              ?.employmentDetails?.departmentName,
+            id: response.data.result?.response?.content[0]?.profileDetails
+              ?.employmentDetails?.osid,
+          },
+          position:
+            response.data.result?.response?.content[0]?.profileDetails
+              ?.professionalDetails?.designation,
+          dateOfJoining:
+            response.data.result?.response?.content[0]?.profileDetails
+              ?.professionalDetails?.doj,
+          roles:
+            response.data.result?.response?.content[0]?.organizations[0].roles,
+        },
+      };
     } else {
       // Handle other status codes or errors
       throw new Error('Failed to fetch user data');
